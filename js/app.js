3c12cfefdb43c9354906ec9d8d0d90fe73b07943
EddMoi06 <145163602+EddMoi06@users.noreply.github.com>
2024-01-03 14:22:36 -0400

    modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -16,6 +16,16 @@ class Presupuesto {
         this.restante = Number(presupuesto);
         this.gastos = [];
     }
+
+    nuevoGasto(gasto){
+        this.gastos = [...this.gastos, gasto];
+        this.calcularRestante()
+    }
+
+    calcularRestante(){
+        const gastado = this.gastos.reduce( (total, gasto) => total + gasto.cantidad, 0)
+        this.restante = this.presupuesto - gastado;
+    }
 }
 
 class UI {
@@ -38,7 +48,7 @@ class UI {
             if(tipo === 'error'){
                 alerta.classList.add('alert-danger')
             }else{
-                alerta.classList.add('alert-succes')
+                alerta.classList.add('alert-success')
             }
 
             document.querySelector('.primario').insertBefore(alerta, formulario)
@@ -46,6 +56,67 @@ class UI {
             setTimeout(() => {
                 alerta.remove()
             }, 3000);
+        }
+    }
+
+    actualizarPresupuesto(restante){
+        document.querySelector('#restante').textContent = restante
+    }
+
+    comprobarPresupuesto(gastos){
+        const {restante, presupuesto} = gastos;
+        const restanteDiv = document.querySelector('.restante')
+
+        if((presupuesto / 4) > restante){
+            restanteDiv.classList.add('alert-danger')
+            restanteDiv.classList.remove('alert-success', 'alert-warning')
+        } else if((presupuesto / 2) > restante){
+            restanteDiv.classList.add('alert-warning')
+            restanteDiv.classList.remove('alert-success', 'alert-danger')
+        } else{
+            restanteDiv.classList.add('alert-success')
+            restanteDiv.classList.remove('alert-warning', 'alert-danger')
+        }
+
+        if(restante <= 0 ){
+            ui.mensajeAlerta('Se Agoto el presupuesto', 'error')
+            document.querySelector('.btn-primary').disabled = true
+        }else{
+            document.querySelector('.btn-primary').disabled = false
+        }
+    }
+
+    agregarGastoListado(gastos){
+        this.limpiarHTML()
+        gastos.forEach(gasto => {
+            const {cantidad, nombre, id} = gasto;
+
+            const nuevoGasto = document.createElement('li');
+            nuevoGasto.className = 'list-group-item d-flex justify-content-between align-items-center';
+            nuevoGasto.dataset.id = id;
+
+            nuevoGasto.innerHTML = `
+                ${nombre} <span class="badge badge-pill badge-primary">$ ${cantidad}</span>
+            `;
+
+            const btnBorrar = document.createElement('button');
+            btnBorrar.textContent = 'Borrar';
+            btnBorrar.className = 'btn btn-danger borrar-gasto'
+            
+            
+
+            nuevoGasto.appendChild(btnBorrar)
+            gastoListado.appendChild(nuevoGasto)
+        })
+    }
+
+    eliminarGasto(id){
+        console.log(gastoListado)
+    }
+
+    limpiarHTML(){
+        while(gastoListado.firstChild){
+            gastoListado.removeChild(gastoListado.firstChild)
         }
     }
     
@@ -72,7 +143,7 @@ function agregarGasto(e){
     e.preventDefault()
 
     const nombre = document.querySelector('#gasto').value;
-    const cantidad = document.querySelector('#cantidad').value;
+    const cantidad = parseInt(document.querySelector('#cantidad').value);
 
     if(nombre === '' || cantidad === ''){
         ui.mensajeAlerta('Ambos campos son obligatorios', 'error')
@@ -82,6 +153,14 @@ function agregarGasto(e){
         return
     }
 
+    ui.mensajeAlerta('Correcto')
+    formulario.reset()
+
     const detallesGasto = {nombre, cantidad, id: Date.now()}
-    console.log(detallesGasto)
+    presupuesto.nuevoGasto(detallesGasto)
+
+    const { gastos, restante } = presupuesto
+    ui.agregarGastoListado(gastos)
+    ui.actualizarPresupuesto(restante)
+    ui.comprobarPresupuesto(presupuesto)
 }
